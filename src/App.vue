f0cb34b91b2219d6493019adfb477870a0cadddf
DominikKysel <kysel.dominik@gmail.com>
2023-11-26 15:19:08 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <h1>Vue 3 + Vite + Vue Router</h1>
+      <nav>
+        <RouterLink to="/">Gallery</RouterLink>
+        <RouterLink to="/Map">Map</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/GalleryView.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+
+
+</script>
+
+<template>
+  <main class="container">
+    <h1>Gallery</h1>
+  </main>
+</template>
